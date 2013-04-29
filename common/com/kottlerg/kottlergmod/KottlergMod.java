24ef781a84bd97716075d3bc0891b9ac628057a3
kottlerg <dev.9339@hotmail.com>
2013-04-29 15:51:12 +0800

    Commit_01

---
+++ common/com/kottlerg/kottlergmod/KottlergMod.java
@@ -0,0 +1,30 @@
+package com.kottlerg.kottlergmod;
+
+import com.kottlerg.kottlergmod.lib.Referance;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Referance.MOD_ID, name = Referance.MOD_NAME, version = Referance.VERSION)
+public class KottlergMod {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event) {
+        
+    }
+    
+    @Init
+    public void Init(FMLInitializationEvent event) {
+        
+    }
+    
+    @PostInit
+    public void PostInit(FMLPostInitializationEvent event) {
+        
+    }
+
+}
---
+++ common/com/kottlerg/kottlergmod/lib/Referance.java
@@ -0,0 +1,9 @@
+package com.kottlerg.kottlergmod.lib;
+
+public class Referance {
+
+    public static final String MOD_ID = "kottlergmod";
+    public static final String MOD_NAME = "Kottlerg Mod";
+    public static final String VERSION = "0.0.1";
+    
+}
