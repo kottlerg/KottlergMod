8eb0c5f0d9305510ad7614a5243788c07e49461d
kottlerg <dev.9339@hotmail.com>
2013-04-29 19:09:20 +0800

    Commit_03

--- common/com/kottlerg/kottlergmod/block/OreAluminum.java
+++ common/com/kottlerg/kottlergmod/block/OreAluminum.java
@@ -2,6 +2,7 @@ package com.kottlerg.kottlergmod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class OreAluminum extends Block {
@@ -11,4 +12,7 @@ public class OreAluminum extends Block {
         this.setCreativeTab(CreativeTabs.tabBlock);
     }
     
+    public void registerIcon(IconRegister reg){
+        this.blockIcon = reg.registerIcon("KottlergMod:AluminumOre");
+    }
 }
